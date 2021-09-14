64daf7c69e1f7eb06adb8c9253a14792e0559ee7
Julien Alardot <alardot.j@gmail.com>
2021-09-14 18:59:07 +0200

    Chapter recovery and reformatting

--- app/app.py
+++ app/app.py
@@ -11,36 +11,35 @@ DATAPATH = os.path.join(PATH, "data")
 
 
 def get_book_id():
-    selection = books[books['Title'] == title]['Text#']
+    selection = books[books["Title"] == title]["Text#"]
     return selection.iloc[0]
 
 
-books = pd.read_csv(os.path.join(DATAPATH, "pg_catalog.csv"), sep=',', dtype={
-    "Text#":np.int32,
-    "Type": "category",
-    "Language": "category"})
-titles = books['Title']
+books = pd.read_csv(
+    os.path.join(DATAPATH, "pg_catalog.csv"),
+    sep=",",
+    dtype={"Text#": np.int32, "Type": "category", "Language": "category"},
+)
+titles = books["Title"]
 titles.drop_duplicates(inplace=True)
 titles.dropna(inplace=True)
 
-st.title('Summarizer')
+st.title("Summarizer")
 
-title = st.sidebar.selectbox(
-    'Which book do you want?',
-    titles)
+title = st.sidebar.selectbox("Which book do you want?", titles)
 
-'You selected:', title
+"You selected:", title
 
 left_column, right_column = st.columns(2)
-pressed = left_column.button('confirm')
+pressed = left_column.button("confirm")
 if pressed:
     book_id = str(get_book_id())
     url = f"https://www.gutenberg.org/files/{book_id}/{book_id}-h/{book_id}-h.htm"
 
     r = requests.get(url)
     fichiers = os.listdir(DATAPATH)
-    if book_id+'.html' not in fichiers:
-        with open(os.path.join(DATAPATH, book_id+'.html'), 'w') as file:
+    if book_id + ".html" not in fichiers:
+        with open(os.path.join(DATAPATH, book_id + ".html"), "w") as file:
             file.write(r.text)
 
 expander = st.expander("FAQ")
--- app/core/extract_html.py
+++ app/core/extract_html.py
@@ -4,13 +4,13 @@ from bs4 import BeautifulSoup
 class BreakDownBook:
     def __init__(self, html_filepath):
         with open(html_filepath, "r") as html:
-            self.soup = BeautifulSoup(html, features="html.parser").find("body")
+            self.soup = BeautifulSoup(html, features="html.parser")
 
-        now = self.soup.findNext("div", attrs=dict(style="display:block; "
-                                                         "margin-top:1em; "
-                                                         "margin-bottom:1em; "
-                                                         "margin-left:2em; "
-                                                         "text-indent:-2em"))
+        now = self.soup.find("body").findNext("div", attrs=dict(style="display:block; "
+                                                                      "margin-top:1em; "
+                                                                      "margin-bottom:1em; "
+                                                                      "margin-left:2em; "
+                                                                      "text-indent:-2em"))
         title = now.text.replace("\n", " ").replace("\t", " ")
         self.title = " ".join([l for l in title.split(" ") if l]).split(": ")[-1]
         now = now.findNext("div", attrs=dict(style="display:block; "
@@ -26,13 +26,19 @@ class BreakDownBook:
         chapters = [" ".join(x for x in element.text.replace("\n", " ").replace("\t", " ").split(" ")
                              if x).split(". ")[-1]
                     for element in table.find_all("tr")]
-        self.chapters = tuple(chapters)
+        self.chapter_names = tuple(chapters)
+        self.chapters = list()
+        for chapter in self.chapter_names:
+            now = now.findNext("div", class_="chapter")
+            chapter = "\n".join([x.text.lower().replace("\n", " ").replace("\t", " ") for x in now.findAll("p")])
+            chapter = " ".join(x for x in chapter.split(" ") if x).replace(" \n", "\n")
+            self.chapters.append(chapter)
+        self.chapters = tuple(self.chapters)
 
     @property
     def n_chapters(self):
-        return len(self.chapters)
+        return len(self.chapter_names)
 
 
 if __name__ == "__main__":
     b = BreakDownBook("../../app/data/103.html")
-    print(b.chapters)
