d80f536cbe7dc9e2a6efd11e063aab4008a526bc
Julien Alardot <alardot.j@gmail.com>
2021-09-14 15:02:01 +0200

    Edit requirements, app.py and test_app.py

--- app/app.py
+++ app/app.py
@@ -1,29 +1,32 @@
 TEST_VALUE = "Hello World!"
 
-import streamlit as st
+import os
+
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
-import numpy as np
 import pandas as pd
+import streamlit as st
 
-books = pd.read_csv("./data/pg_catalog.csv", sep=',')
+PATH = os.path.abspath(os.path.dirname(__file__))
+DATAPATH = os.path.join(PATH, "data")
+
+books = pd.read_csv(os.path.join(DATAPATH, "pg_catalog.csv"), sep=',')
 titles = books['Title']
-titles.drop_duplicates(inplace = True)
-titles.dropna(inplace = True)
+titles.drop_duplicates(inplace=True)
+titles.dropna(inplace=True)
 
 st.title('Summarizer')
 
 option = st.sidebar.selectbox(
     'Which number do you like best?',
-     titles)
+    titles)
 
 'You selected:', option
 
 left_column, right_column = st.columns(2)
 pressed = left_column.button('Press me?')
 if pressed:
-  right_column.write("Woohoo!")
+    right_column.write("Woohoo!")
 
 expander = st.expander("FAQ")
 expander.write("Here you could put in some really, really long explanations...")
-
--- unittesting/test_app.py
+++ unittesting/test_app.py
@@ -1,8 +1,22 @@
-from app.app import TEST_VALUE
+import os
 import unittest
+import time
+from app.app import TEST_VALUE
+
+PATH = os.path.dirname(__file__)
+ROOTPATH = os.path.dirname(PATH)
+
+
 class TestApp(unittest.TestCase):
     def test_variables(self):
         self.assertEqual("Hello World!", TEST_VALUE)
+
+    # def test_streamlit(self):
+    #     os.system(f'cd {ROOTPATH}')
+    #     os.system("streamlit run app/app.py")
+    #     time.sleep(10)
+    #     os.system("^C")
+
 
 if __name__ == "__main__":
     unittest.main()
