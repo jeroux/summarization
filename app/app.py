9601803134c2c8171e781764768d6b5709f97e97
Julien Alardot <alardotj.pro@gmail.com>
2021-09-20 16:09:39 +0200

    Switching to streamlit caching and form

--- app/app.py
+++ app/app.py
@@ -10,41 +10,47 @@ from core.ml.qamodel import QABookSummerizerML
 PATH = os.path.abspath(os.path.dirname(__file__))
 DATAPATH = os.path.join(PATH, "data")
 
-
-def get_book_id():
+@st.cache
+def get_book_id(books):
     selection = books[books["Title"] == title]["Text#"]
     return selection.iloc[0]
 
+@st.cache
+def get_books_data():
+    books = pd.read_csv(
+        os.path.join(DATAPATH, "pg_catalog.csv"),
+        sep=",",
+        dtype={"Text#": np.int32, "Type": "category", "Language": "category"}, low_memory=False
+    )
+    books = books[books["Type"] == "Text"]
+    titles = books["Title"]
+    titles.drop_duplicates(inplace=True)
+    titles.dropna(inplace=True)
+    return books, titles
 
-books = pd.read_csv(
-    os.path.join(DATAPATH, "pg_catalog.csv"),
-    sep=",",
-    dtype={"Text#": np.int32, "Type": "category", "Language": "category"}, low_memory=False
-)
-books = books[books["Type"] == "Text"]
-titles = books["Title"]
-titles.drop_duplicates(inplace=True)
-titles.dropna(inplace=True)
+books, titles = get_books_data()
 
 st.title("Summarizer")
 
-title = st.sidebar.selectbox("Which book do you want?", titles)
+with st.sidebar:
+
+    with st.form(key='my_form'):
+        title = st.form.selectbox("Which book do you want?", titles)
+        st.form("How many chapters do you want?", min_value=1, max_value=1000, value=1, step=1, key="nb_chapitres")
+        submit_button = st.form_submit_button(label='Submit parameters')
+
+
 
 "You selected:", title
 
-st.slider("How many chapters do you want?", min_value=1, max_value=1000, value=1, step=1, key="nb_chapitres")
-
-
-left_column, right_column = st.columns(2)
-pressed = left_column.button("confirm")
 expander = st.expander("Bert")
 expander.write("Here will come the Bart summary")
 expander2 = st.expander("GPT")
 expander3 = st.expander("XLM")
 expander4 = st.expander('FAQ')
 
-if pressed:
-    book_id = str(get_book_id())
+if submit_button:
+    book_id = str(get_book_id(books))
     url = f"https://www.gutenberg.org/files/{book_id}/{book_id}-h/{book_id}-h.htm"
 
     r = requests.get(url)
