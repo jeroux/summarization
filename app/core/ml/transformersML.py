d1ac62464802996e0094f2bdab3a90c5d450003d
jeroux <jeremylipszyc@gmail.com>
2021-09-20 14:43:32 +0200

    add Bert, GPT2 and XLM

--- app/core/ml/transformersML.py
+++ app/core/ml/transformersML.py
@@ -1,28 +1,19 @@
-import json
-import os
-
-from transformers import BartForConditionalGeneration, BartTokenizer
-
-from core.extract_html import BreakDownBook
+from summarizer import Summarizer, TransformerSummarizer
 
 
 class Bert:
-    def __init__(self):
-        self.tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-        self.model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
-
-    def summarize(self, text):
-        inputs = self.tokenizer.batch_encode_plus([text],
-                                             return_tensors='pt',
-                                             max_length=1024,
-                                             truncation=True)
-        summary_ids = self.model.generate(inputs['input_ids'], early_stopping=True)
-        return self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    def __init__(self, text):
+        model = Summarizer()
+        self.summary = ''.join(model(text, min_length=20, max_length=30))
 
 
-class GPT2():
-    pass
+class GPT2:
+    def __init__(self, text):
+        model = TransformerSummarizer(transformer_type="GPT2", transformer_model_key="gpt2-medium")
+        self.summary = ''.join(model(text, min_length=20, max_length=30))
 
 
-class XLM():
-    pass
+class XLM:
+    def __init__(self, text):
+        model = TransformerSummarizer(transformer_type="XLNet", transformer_model_key="xlnet-base-cased")
+        self.summary = ''.join(model(text, min_length=20, max_length=30))
