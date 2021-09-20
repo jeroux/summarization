2ede6c6717545db11d3cabcc885c0b63f33df671
Julien Alardot <alardotj.pro@gmail.com>
2021-09-20 16:31:20 +0200

    Removing old models

--- app/DockerPreLoadModels.py
+++ app/DockerPreLoadModels.py
@@ -1,6 +1,4 @@
-from transformers import BartForConditionalGeneration, AutoTokenizer, BartTokenizer, AutoModelForQuestionAnswering
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 
-BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
 AutoTokenizer.from_pretrained('valhalla/bart-large-finetuned-squadv1')
 AutoModelForQuestionAnswering.from_pretrained("valhalla/bart-large-finetuned-squadv1")
