3def557c1429f8bf53642cf62aba319a6cb50e67
Julien Alardot <alardotj.pro@gmail.com>
2021-09-20 16:30:50 +0200

    Better chapter concatenation

--- app/core/extract_html.py
+++ app/core/extract_html.py
@@ -21,7 +21,7 @@ class BreakDownBook:
 
     @property
     def text(self):
-        return "\n".join(self.chapters)
+        return " ".join(self.chapters)
 
     def get_text_until_n_chapter(self, n):
         return "\n".join(self.chapters[:n])
